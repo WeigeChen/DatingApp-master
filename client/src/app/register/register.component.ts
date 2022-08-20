a34f0adefa0b1a9853ed48b2bd0c64cd9cce5724
WeigeChen <godwin.chen@gmail.com>
2022-08-19 22:18:10 -0400

    section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, Observer, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  
+  @Output() cancelRegister = new EventEmitter();
+  model: any ={};
+
+
+  constructor(private accountService :AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    // console.log(this.model);
+    // this.accountService.register(this.model).pipe(
+      
+    //   map((response)=>{
+    //      console.log(response);
+    //      alert('test');
+    //     this.cancel();
+    //     // return response;
+    //   }
+    //   ))
+
+    this.accountService.register(this.model).subscribe(
+    (response) =>{console.log(response),
+     this.cancel();
+    }
+    ,(error)=>{console.log(error)}
+    )
+    
+    
+  }
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+
+}
